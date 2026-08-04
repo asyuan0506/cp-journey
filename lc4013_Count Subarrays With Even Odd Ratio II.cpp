a41b433428da7e3e4d9f0f460e3495ce62637f70
asyuan0506 <asyuan0506@gmail.com>
2026-08-04 14:06:51 +0800

    Binary Indexed Tree

--- lc4013_Count Subarrays With Even Odd Ratio II.cpp
+++ lc4013_Count Subarrays With Even Odd Ratio II.cpp
@@ -51,3 +51,55 @@ public:
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     vector<long long> BIT;
+//     long long Query(int x)
+//     {
+//         long long sum = 0;
+//         for (; x < BIT.size(); x += x & (-x))
+//             sum += BIT[x];
+//         return sum;
+//     }
+//     void Update(int x)
+//     {
+//         for (; x > 0; x -= x & (-x))
+//             BIT[x]++;
+//     }
+//     long long countRatioSubarrays(vector<int>& nums, int a, int b) {
+//         const int n = nums.size();
+//         long long pre_x = 0;
+//         long long pre_y = 0;
+//         vector<long long> diff(n + 1);
+//         diff[0] = 0;
+//         long long ans = 0;
+//         for (int i = 0; i < n; i++)
+//         {
+//             pre_x = pre_x + (nums[i] % 2 == 0) * b;
+//             pre_y = pre_y + (nums[i] % 2) * a;
+//             diff[i + 1] = pre_x - pre_y;
+//             // for (int j = 0; j <= i; j++)
+//             // {
+//             //     // pre_x[i + 1] - pre_y[i + 1] <= pre_x[j] - pre_y[j]
+//             //     // for every i count how many >= pre_x[i + 1] - pre_y[i + 1]
+//             //     if (diff[j] >= diff[i + 1]) ans++;
+//             // }
+//         }
+//         vector<long long> sorted = diff;
+//         sort(sorted.begin(), sorted.end());
+//         unordered_map<long long, int> rank;
+//         for (int i = 0; i <= n; i++)
+//             rank[sorted[i]] = i + 1;
+
+//         BIT.assign(diff.size() + 2, 0);
+//         for (long long value : diff)
+//         {
+//             int rk = rank[value];
+//             ans += Query(rk);
+//             Update(rk);
+//         }
+        
+//         return ans;
+//     }
+// };
